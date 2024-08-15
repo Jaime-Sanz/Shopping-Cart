80e84d09c6be54b6f7bcf2ac1ad2ee6029a4ef89
Jaime Sanchez <118962371+Jaime-Sanz@users.noreply.github.com>
2024-08-15 04:01:00 -0700

    fin

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,14 @@
 import logo from '../assets/images/icon-shoppingcart.png';
 import styles from '../styles/Cart.module.css';
+import { useState } from 'react';
 
 const Cart = (cart) => {
 
+    const [cartVisibility, setCartVisibility] = useState(false);
+
     return (
         <div>
+            <div className={styles.cartContainer}></div>
             <button className={styles.cartButton}><img src={logo} alt="Your Cart" /> Cart</button>
         </div>
     );
--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -4,34 +4,51 @@ import jewelery from "../assets/images/icon-jewelery.png";
 import man from "../assets/images/icon-man.png";
 import women from "../assets/images/icon-women.png";
 import rating from "../assets/images/icon-rating.png";
-import Cart from "../components/Cart";
+import shoppingCart from "../assets/images/icon-shoppingcart.png";
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-
 const Shopping = () => {
-
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [cartState, setCartState] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('all');
   const [cart, setCart] = useState([]);
+  const [toggleCart, setToggleCart] = useState(false);
 
-  const addItemCart = (product) => {
+  const addItemCart = (product, quantity) => {
     setCart((prevCart) => {
-      const updatedCart = [...prevCart, product];
-      return updatedCart;
+      const existingProductIndex = prevCart.findIndex(item => item.id === product.id);
+      
+      if (existingProductIndex >= 0) {
+        const updatedCart = [...prevCart];
+        updatedCart[existingProductIndex] = {
+          ...updatedCart[existingProductIndex],
+          quantity: updatedCart[existingProductIndex].quantity + quantity
+        };
+        return updatedCart;
+      } else {
+        return [...prevCart, { ...product, quantity }];
+      }
     });
+  };
+
+  const handleAddToCart = (product) => {
+    const quantity = parseInt(document.querySelector(`#quantity-${product.id}`).value, 10) || 1;
+    addItemCart(product, quantity);
+  };
+
+  const toggleCartState = () => {
+    setToggleCart((prevState) => !prevState);
   };
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
     .then((response) => {
       if(!response.ok) {
-        throw new Error('Network Error: Products Failed To Load')
+        throw new Error('Network Error: Products Failed To Load');
       }
-      return response.json()
+      return response.json();
     })
     .then((products) => {
       setProducts(products);
@@ -45,66 +62,82 @@ const Shopping = () => {
 
   const handleCategoryChange = (category) => {
     setSelectedCategory(category);
-  }
+  };
 
   const filteredProducts = selectedCategory === 'all'
     ? products
     : products.filter((product) => product.category === selectedCategory);
 
+  const totalPrice = cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+
   if(loading) {
     return <div className={styles.loadingContainer}>
               <div className={styles.loader}></div>
-            </div>
+            </div>;
   }
 
   if(error) {
-    return <div className={styles.errorMessage}>{error}</div>
+    return <div className={styles.errorMessage}>{error}</div>;
   }
 
-    return (
-      <div className={styles.container}>
-        <div className={styles.sideBar}>
-          <h1 className={styles.headers}>Categories</h1>
-          <li className={styles.categories}>
-            <button onClick={() => handleCategoryChange('electronics')}><img src={electronic} alt="logo for electronics" />Electronics</button>
-            <button onClick={() => handleCategoryChange('jewelery')}><img src={jewelery} alt="logo for jewelery" />Jewelery</button>
-            <button onClick={() => handleCategoryChange("men's clothing")}><img src={man} alt="logo for men" />Men</button>
-            <button onClick={() => handleCategoryChange("women's clothing")}><img src={women} alt="logo for women" />Women</button>
-          </li>
-          <button className={styles.resetButton} onClick={() => handleCategoryChange('all')}>Reset Filter</button>
-          <div className={styles.cartContainer}>
-            <Cart  cartInformation={cart} onClick={cartState}/>
-            <div className={styles.cartBox}></div>
-          </div>
-        </div>
-        <div className={styles.shoppingContainer}>
-            {filteredProducts.map((product) => (
-              <div key={product.id} className={styles.card}>
-                  <h4>{product.title}</h4>
-                  <img src={product.image} alt="picture of product" className={styles.productImg}/>
-                  <div className={styles.rating}>
-                    <p>Rating: {product.rating.rate} <img src={rating} alt="picture of stars"/></p>
-                  </div>
-                  <p className={styles.price}>{"$" + product.price}</p>
-                  <div className={styles.priceButtonContainer}>
-                    <button onClick={() => addItemCart(product)}>Add To Cart</button>
-                    <input type="number" min="1" max="100" placeholder="1"/>
-                  </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.sideBar}>
+        <div className={styles.toggleCart} style={{ visibility: toggleCart ? 'visible' : 'hidden' }}>
+          <div>
+            {cart.map((item) => (
+              <div key={item.id} className={styles.cartItem}>
+                <img src={item.image} alt={item.title} className={styles.cartItemImage} />
+                {item.title} - Quantity: {item.quantity} - ${item.price * item.quantity}
               </div>
             ))}
+          </div>
+          <div className={styles.cartTotal}>
+            <p>Total: ${totalPrice.toFixed(2)}</p>
+          </div>
+          <div className={styles.toggleCartButtonContainer}>
+            <button className={styles.toggleCartButton} onClick={toggleCartState}>Close Cart</button>
+            <button className={styles.toggleCartButton}>Check Out</button>
+          </div>
         </div>
+        <h1 className={styles.headers}>Categories</h1>
+        <li className={styles.categories}>
+          <button onClick={() => handleCategoryChange('electronics')}><img src={electronic} alt="logo for electronics" />Electronics</button>
+          <button onClick={() => handleCategoryChange('jewelery')}><img src={jewelery} alt="logo for jewelery" />Jewelery</button>
+          <button onClick={() => handleCategoryChange("men's clothing")}><img src={man} alt="logo for men" />Men</button>
+          <button onClick={() => handleCategoryChange("women's clothing")}><img src={women} alt="logo for women" />Women</button>
+        </li>
+        <button className={styles.resetButton} onClick={() => handleCategoryChange('all')}>Reset Filter</button>
+        <button className={styles.cartButton} onClick={toggleCartState}><img src={shoppingCart} alt="Your Cart" /> Cart</button>
       </div>
-    );
-  };
+      <div className={styles.shoppingContainer}>
+        {filteredProducts.map((product) => (
+          <div key={product.id} className={styles.card}>
+            <h4>{product.title}</h4>
+            <img src={product.image} alt="picture of product" className={styles.productImg}/>
+            <div className={styles.rating}>
+              <p>Rating: {product.rating.rate} <img src={rating} alt="picture of stars"/></p>
+            </div>
+            <p className={styles.price}>{"$" + product.price}</p>
+            <div className={styles.priceButtonContainer}>
+              <button onClick={() => handleAddToCart(product)}>Add To Cart</button>
+              <input type="number" min="1" max="100" placeholder="1" id={`quantity-${product.id}`}/>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-  Shopping.propTypes = {
-    products: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-      })
-    ).isRequired,
-  };
-  
-  export default Shopping;
+Shopping.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Shopping;
